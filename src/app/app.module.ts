40457aa9e2b3d5330e158a40c1694ba88d254c6d
ajay yadav <developper.ajay@gmail.com>
2021-09-12 13:45:55 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { BlogComponent } from './blog/blog.component';
+import { SkillsComponent } from './about/skills/skills.component';
+import { FooterComponent } from './footer/footer.component';
+import { HtmlComponent } from './blog/html/html.component';
+
+
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    BlogComponent,
+    SkillsComponent,
+    FooterComponent,
+    HtmlComponent,
+  
+  
   ],
   imports: [
     BrowserModule,
